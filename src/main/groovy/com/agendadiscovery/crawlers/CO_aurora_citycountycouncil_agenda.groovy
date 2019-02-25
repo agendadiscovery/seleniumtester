b013efb729babfbe9a22ec82dab013f2182572cd
Randall-Mcgoy <randall_abi@outlook.com>
2019-02-25 13:48:13 -0700

    Fixed Aurora CO crawler.  Added working paginate cycler

--- src/main/groovy/com/agendadiscovery/crawlers/CO_aurora_citycountycouncil_agenda.groovy
+++ src/main/groovy/com/agendadiscovery/crawlers/CO_aurora_citycountycouncil_agenda.groovy
@@ -5,15 +5,24 @@ import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.support.ui.ExpectedConditions
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
+import java.time.Year
 import java.util.concurrent.TimeUnit
 
 public class CO_aurora_citycountycouncil_agenda extends BaseCrawler{
+    private static final Logger log = LoggerFactory.getLogger(this.class)
+    int current_year = Year.now().getValue()
+    int paginateIndex = 1
+    boolean hasNext = true
+    List <WebElement> docList = []
 
     // http://chromedriver.chromium.org/getting-started
     public List getDocuments(String baseUrl) throws Exception {
-        log.debug("Starting AZ Youngtown Selenium crawl")
-        log.debug("Requesting baseURL: "+baseUrl)
+        log.info("Starting AZ Youngtown Selenium crawl")
+        log.info("Requesting baseURL: "+baseUrl)
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS)
 
         try {
             driver.get(baseUrl)
@@ -22,8 +31,17 @@ public class CO_aurora_citycountycouncil_agenda extends BaseCrawler{
             By firstPdfIcon = By.xpath("//li[2]/a[1]/div/div[2]/div/em[contains(@class,\"fa-file-pdf-o\")]")
             wait.until(ExpectedConditions.presenceOfElementLocated(firstPdfIcon))
 
-            By rightArrowBy = By.xpath("//*[@id=\"PO-documentContainer_60273\"]/div[2]/div[7]/ul/li[7]/a/em")
-            boolean hasNext = true
+            //Grab paginate buttons
+            String paginatorsPath = "//div[@class=\"PO-paging\"]//li//a[contains(@class,\"pageButton number\")]"
+            List <WebElement> paginators = driver.findElementsByXPath(paginatorsPath)
+            List <String> paginatorsText = []
+            int index = 0;
+            for(WebElement paginator : paginators){
+                if(paginator.getText().matches("^[0-9]+"))  //remove blank buttons
+                    {paginatorsText[index] = paginator.getText()}
+                //log.debug(paginatorsText[index])
+                index++
+            }
 
             while(hasNext){
                 By rowSelector = By.xpath("//ul[@id=\"documentList\"]/li[position()>1]")
@@ -43,27 +61,27 @@ public class CO_aurora_citycountycouncil_agenda extends BaseCrawler{
                     doc.dateStr = row.findElement(dateBy).getText()
                     doc.link = row.findElement(urlBy)?.getAttribute("href")
 
-                    log.debug("\tTitle: ${doc.title}")
-                    log.debug("\tDate: ${doc.dateStr}")
-                    log.debug("\tUrl: ${doc.link}")
+//                    log.debug("\tTitle: ${doc.title}")
+//                    log.debug("\tDate: ${doc.dateStr}")
+//                    log.debug("\tUrl: ${doc.link}")
 
                     docList.add(doc)
                 }
-
-                // Grab right arrow button
-                WebElement rightArrowWE = driver.findElement(rightArrowBy)
-
-                // Sometimes the arrow is hidden
-                if(rightArrowWE?.isDisplayed()){
-                    // Click the right arrow
-                    rightArrowWE.click()
+                // click paginate button (1..2..3)
+                if(paginateIndex < paginatorsText.size()){
+                    WebElement paginator = driver.findElement(By.linkText(paginatorsText[paginateIndex]))
+                    paginator.click()
+                    //log.info("click")
                     sleep(5000)
                 }else{ // no more arrow
                     hasNext = false
+                    // log.info("no click")
                 }
+                paginateIndex++
+                // log.debug(paginateIndex.toString())
             }
         } catch (Exception e) {
-            log.error("Selenium crawl error: "+e)
+            log.info("Selenium crawl error: "+e)
             e.printStackTrace(System.out)
         } finally{
             driver.quit()
